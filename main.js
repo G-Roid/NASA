a15280ab552b86cd8cbc66415974543c810b8c14
Geoffroy Guillotte <geoffroy.guillotte@gmail.com>
2022-05-11 18:14:43 +1000

    added feature to search for custom date

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
-fetch('https://api.nasa.gov/planetary/apod?api_key=KBg3bYzYu9n5Qedy4T5EQFzdDx3pJ3chwgtwJIf5')
-.then(res => res.json())
-.then(data => {
-    console.log(data)
-    document.querySelector('img').src = data.url;
-    document.querySelector('p').textContent = data.explanation
-})
+// fetch('https://api.nasa.gov/planetary/apod?api_key=KBg3bYzYu9n5Qedy4T5EQFzdDx3pJ3chwgtwJIf5')
+// .then(res => res.json())
+// .then(data => {
+//     console.log(data)
+//     document.querySelector('img').src = data.url;
+//     document.querySelector('p').textContent = data.explanation
+// })
+
+document.querySelector('button').addEventListener('click', space)
+
+function space() {
+
+    let date = document.querySelector('input').value;
+
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=KBg3bYzYu9n5Qedy4T5EQFzdDx3pJ3chwgtwJIf5&date=${date}&thumbs=true`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)    
+
+
+        if (data.media_type === 'video') {
+            document.querySelector('img').src = data.thumbnail_url;
+            // document.querySelector('p').textContent = data.explanation
+        } else {
+            document.querySelector('img').src = data.url;
+            // document.querySelector('p').textContent = data.explanation
+
+        }    
+        document.querySelector('p').textContent = data.explanation
+    })
+}
